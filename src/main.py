c53bb01e2a3c9e7b3a4a33a0c9c22db1ae031b3e
AarushKapoor <aarushkapoor187@gmail.com>
2026-02-17 14:25:39 -0500

    Update main.py
    main.py now handles camera input by passing it to hands.py for mediapipe processing. This will get the mediapipe data.
    
    Next steps are to process this mediapipe data

--- src/main.py
+++ src/main.py
@@ -1,1 +1,17 @@
+from src.hand_tracking.camera import Camera
+from src.hand_tracking.hands import HandTracker
+import cv2
 
+camera = Camera()
+tracker = HandTracker()
+
+while True:
+    frame = camera.get_frame()
+    results, annotated_frame = tracker.process(frame)
+
+    cv2.imshow("AuraBeat", annotated_frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+camera.release()
